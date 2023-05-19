192f643dfd31cd65b006f52921e2b5922c939216
Ton Cao <me@toncao.com>
2023-05-19 19:17:56 +0100

    add thanhnien.vn

--- scrap.py
+++ scrap.py
@@ -7,7 +7,7 @@ class scrap():
         self.count = count
 
     def vnexpress(self):
-        url = 'https://vnexpress.net/'
+        url = 'https://vnexpress.net'
         soup = BeautifulSoup(requests.get(url).content, 'html5lib').find_all('p', attrs = {'class': 'description'})
         news =[{'title': content.a['title'],
                 'description': content.a.string,
@@ -15,9 +15,17 @@ class scrap():
         return news
 
     def tuoitre(self):
-        url = 'https://tuoitre.vn/'
+        url = 'https://tuoitre.vn'
         soup = BeautifulSoup(requests.get(url).content, 'html5lib').find_all('a', attrs = {'class': 'box-category-link-title'})
         news =[{'title': content['title'],
                 'description': None,
                 'link': url + content['href']} for content in soup[:min(self.count,len(soup))]]
+        return news
+
+    def thanhnien(self):
+        url = 'https://thanhnien.vn'
+        soup = BeautifulSoup(requests.get(url).content, 'html5lib').find_all('a', attrs = {'class': 'box-category-link-title'})
+        news =[{'title': content['title'],
+                'description': None,
+                'link': url + content['href']} for content in soup[3:min(self.count+3,len(soup))]]
         return news
